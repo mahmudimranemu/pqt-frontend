b87fd648c65d66fcdab9f10a686b96d5286f752b
Mahmud Imran <mahmudimranemu@gmail.com>
2025-12-06 20:55:19 +0600

    error solve

--- app/(public)/blogs/page.tsx
+++ app/(public)/blogs/page.tsx
@@ -1,4 +1,5 @@
 import { fetchBlogs } from "@/lib/wpapi";
+export const dynamic = "force-dynamic";
 import Link from "next/link";
 import Image from "next/image";
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
